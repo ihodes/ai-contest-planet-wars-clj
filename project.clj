d2a6401c2901c1319345dfd5405f835df04ec832
Isaac Hodes <ihodes@mac.com>
2010-09-20 16:35:36 -0500

    Changed lein compile target to MyBot from cljbot

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-pwars "1.0.0-SNAPSHOT"
-  :description "Clojure starter package for planet-wars Google ai contest."
+  :description "Clojure starter package for the Planet-Wars Google AI Contest."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :uberjar-name "cljbot.jar"
+  :uberjar-name "MyBot.jar"
   :main pw.bot)
